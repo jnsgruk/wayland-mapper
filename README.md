b4b687ac56940aa275c2d1f3654c6c08e0ca6d7f
Jon Seager <jonseager@gmail.com>
2019-08-17 18:47:11 +0100

    Stopped commands running in shell mode

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ### Wayland Mouse Mapper
 
-Maps mouse buttons and gestures to keyboard combinations. Check out the first few lines for configuration information.
+Maps mouse buttons and touchpad gestures to keyboard combinations. Check out the first few lines for configuration information. I wrote this mostly for personal use, I like mapping the side buttons on my mouse to GNOME's Overview Mode... I also like accessing the overview mode with touchpad gestures
+
+This tool makes no promises about security/performance, but with the limited config shown in this repo, it seems to perform pretty well and doesn't consume any noticeable resources.
 
 Some useful commands for understanding your setup are:
 
@@ -13,12 +15,11 @@ Some useful commands for understanding your setup are:
 - `evemu`
 - `node`
 
-### Install
+### Install as a service
 
 ```bash
 $ sudo cp ./main.js /usr/local/bin/wayland-mapper.js
 $ sudo useradd -M -s /sbin/nologin -g input mapper
 $ sudo cp ./wayland-mapper.service /etc/systemd/system/wayland-mapper.service
 $ sudo systemctl enable --now wayland-mapper
-$ journalctl -fu wayland-mapper #check the bad boy is running...
 ```
--- main.js
+++ main.js
@@ -5,53 +5,77 @@ const { spawn } = require("child_process")
 console.log("Starting Wayland mapper...")
 
 const config = {
-  mouse: "event19",
-  keyboard: "event4",
-  touchpad: "event18",
+  mouse: "event19", // Find these by running libinput list-devices
+  keyboard: "event4", // Find these by running libinput list-devices
+  touchpad: "event18", // Find these by running libinput list-devices
   mappings: [
     {
-      type: "mouseButton",
-      sourceEvent: "BTN_EXTRA",
-      targetKeys: ["KEY_LEFTMETA"],
+      type: "mouseButton", // Type of event to respond to
+      sourceEvent: "BTN_EXTRA", // In my case, this is the forward-most side button on my mouse
+      targetKeys: ["KEY_LEFTMETA"], // Keys to press in response
     },
     {
       type: "mouseButton",
-      sourceEvent: "BTN_SIDE",
-      targetKeys: ["KEY_LEFTALT", "KEY_LEFT"],
+      sourceEvent: "BTN_SIDE", // In my case, this is the rear-most side button on my mouse
+      targetKeys: ["KEY_LEFTALT", "KEY_LEFT"], // Keys to press in response
     },
     {
-      type: "gesture",
-      sourceEvent: "3",
-      sourceType: "GESTURE_SWIPE_END",
-      targetKeys: ["KEY_LEFTMETA"],
-      endEvent: "",
+      type: "gesture", // Respond to a touchpad gesture
+      sourceEvent: "3", // 3 fingers (valid values are 3 or 4)
+      sourceType: "GESTURE_SWIPE_END", // This is the event that'sfired when a gesture is deemed over
+      targetKeys: ["KEY_LEFTMETA"], // Keys to press in response
     },
   ],
 }
 
 const keyboard = `/dev/input/${config.keyboard}`
 
-const exec = command => {
-  const result = spawn(command, [], { shell: true })
+// A simple wrapper around the spawn command
+const exec = (command, args) => {
+  const result = spawn(command, args)
   result.stderr.on("data", data => console.error("ERROR: " + data.toString()))
   return result
 }
 
-const events = exec(`stdbuf -oL libinput debug-events`)
-
+// Press a key combination
 const pressKeys = keys => {
+  // Firstly press all the keys...
   keys.map(key => {
-    exec(`evemu-event ${keyboard} --sync --type EV_KEY --code ${key} --value 1`)
+    exec("evemu-event", [
+      keyboard,
+      "--sync",
+      "--type",
+      "EV_KEY",
+      "--code",
+      key,
+      "--value",
+      1,
+    ])
   })
+  // Then release them
   keys.map(key => {
-    exec(`evemu-event ${keyboard} --sync --type EV_KEY --code ${key} --value 0`)
+    exec("evemu-event", [
+      keyboard,
+      "--sync",
+      "--type",
+      "EV_KEY",
+      "--code",
+      key,
+      "--value",
+      0,
+    ])
   })
 }
 
+// Collect data from stdout about libinput events
+const events = exec("stdbuf", ["-oL", "libinput", "debug-events"])
+
+// Process the events...
 events.stdout.on("data", data => {
   const output = data.toString()
   // console.log(output)
   config.mappings.map(({ type, ...args }) => {
+    // for each event, check all of the mappings...
     if (type === "mouseButton") {
       const isPointerButton = output.includes("POINTER_BUTTON")
       const isMappedButton = output.includes(args.sourceEvent)
